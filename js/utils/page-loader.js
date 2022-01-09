7d54028f9b6d6f726b865169c523256ac8331b97
That_HCT_Dev <hctvidchannel@gmail.com>
2022-01-09 21:26:11 +0800

    commit

---
+++ js/utils/page-loader.js
@@ -0,0 +1,8 @@
+app.querySelector('.page-loader', function (el) {
+    const pageLoader = el[0];
+    const hidePageLoader = function () {
+        pageLoader.classList.add('hidden');
+    };
+    window.addEventListener('load', hidePageLoader);
+    // document.addEventListener('DOMContentLoaded', hidePageLoader);
+});
